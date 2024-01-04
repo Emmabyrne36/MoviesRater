4d3bbdeeb7e1653969a6e4bc40dd55229a1476e4
Emma Byrne <emma.byrne@roomex.com>
2024-01-04 16:41:59 +0000

    Add basic create endpoint

---
+++ src/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Interfaces;
+using Movies.Application.Models;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+
+        };
+        
+        await _movieRepository.Create(movie);
+        
+        return Created($"/api/movies/{movie.Id}", new MovieResponse
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            YearOfRelease = movie.YearOfRelease,
+            Genres = movie.Genres.ToList()
+        });
+    }
+}
