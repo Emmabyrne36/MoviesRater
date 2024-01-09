db099ffc52414dd642d30ac936ed601f3ef22487
Emmabyrne36 <emmabyrne36@gmail.com>
2024-01-09 12:13:45 +0000

    Add sorting

--- src/Movies.Application/Validators/GetAllMoviesOptionsValidator.cs
+++ src/Movies.Application/Validators/GetAllMoviesOptionsValidator.cs
@@ -2,11 +2,21 @@
 using Movies.Application.Models;
 
 namespace Movies.Application.Validators;
+
 public class GetAllMoviesOptionsValidator : AbstractValidator<GetAllMoviesOptions>
 {
+    private static readonly string[] AcceptableSortFields =
+    {
+        "title", "yearofrelease"
+    };
+
     public GetAllMoviesOptionsValidator()
     {
         RuleFor(x => x.YearOfRelease)
             .LessThanOrEqualTo(DateTime.UtcNow.Year);
+
+        RuleFor(x => x.SortField)
+            .Must(x => x is null || AcceptableSortFields.Contains(x, StringComparer.OrdinalIgnoreCase))
+            .WithMessage("You can only sort by 'title' or 'yearofrelease'.");
     }
 }
