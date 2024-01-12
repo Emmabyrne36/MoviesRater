ef21b34b359a9833b4f60c2c2e7d90142dbfbd7a
Emmabyrne36 <emmabyrne36@gmail.com>
2024-01-12 16:35:32 +0000

    Add health checks

---
+++ src/Movies.Api/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Movies.Application.Interfaces;
+
+namespace Movies.Api.Health;
+
+public class DatabaseHealthCheck : IHealthCheck
+{
+    public const string HealthCheck = "Database";
+
+    private readonly IDbConnectionFactory _connectionFactory;
+    private readonly ILogger<DatabaseHealthCheck> _logger;
+
+    public DatabaseHealthCheck(IDbConnectionFactory connectionFactory, ILogger<DatabaseHealthCheck> logger)
+    {
+        _connectionFactory = connectionFactory;
+        _logger = logger;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            _ = await _connectionFactory.CreateConnection(cancellationToken);
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception ex)
+        {
+            var errorMessage = "Database is unhealthy";
+            _logger.LogError(errorMessage, ex);
+            return HealthCheckResult.Unhealthy(errorMessage, ex);
+        }
+    }
+}
